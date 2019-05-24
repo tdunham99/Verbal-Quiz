e1f2fe297fbf4866b0ac71d6d3922b8c0efb55aa
System Administrator <root@tristans-air.lan>
2019-05-24 13:59:22 -0700

    Deleted V2 files

--- GameV2.java
+++
@@ -1,7 +0,0 @@
-public class GameV2 {
-
-	public static final double[] sugarlevels = {0, .2, .25, .4, .6, .8, 1.0, 1.2, 1.5, 1.8, 2.0, 2.5, 3.0};
-
-	public static final DrinkV2[] drinks = {new Drink("Coke", "Regular"), sugarlevels[5]}; 
-
-}
